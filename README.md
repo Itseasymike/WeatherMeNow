b0542838279e6185956f9baddb3780ce288fb921
Michael Rivera <mrivera.dev@gmail.com>
2017-01-25 17:30:01 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Weather Me Now
+
+## Instantly know the weather based on your location
---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from '../logo.svg';
+import '../App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+        <div>
+          <h1>Weather Me Now</h1>
+          <h2>Location:</h2>
+          <h2>Forcast:</h2>
+          <h2>Degrees:</h2>
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default App;
